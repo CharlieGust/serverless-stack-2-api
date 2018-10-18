58f60eb1c71fd71813d0fc581ce32fed4836beb2
Charlie Gustavsson <chargust@Charlies-MacBook-Pro.local>
2018-10-18 18:42:04 +0200

    Mistake

--- create.js
+++ create.js
@@ -14,7 +14,7 @@ export async function main(event, context, callback) {
       createdAt: Date.now()
     }
   };
-
+ wharthe.wat;
   try {
     await dynamoDbLib.call("put", params);
     callback(null, success(params.Item));
